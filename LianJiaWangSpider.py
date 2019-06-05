3452363964484f1b21269d8f95cb5cc4e9762a4b
stone <stonewin540@163.com>
2019-06-05 11:44:48 +0800

    场景类整合循环爬虫逻辑；

--- LianJiaWangSpider.py
+++ LianJiaWangSpider.py
@@ -14,31 +14,26 @@ def start():
                  "AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15"
     headers = {'User-Agent': user_agent}
 
-    # 先拆分页面数量数据
-    url = "https://bj.lianjia.com/zufang/"
-
-    req = request.Request(url, headers=headers)
-    response = request.urlopen(req)
-    content = response.read().decode('utf-8')
-    parser = LianJiaWangItemParser()
-    parser.feed(content)
-    parser.close()
-    print('number of pages: %d' % parser.number_of_pages)
-    print('---- page 1 parsed ----\n')
-
-    # number_of_pages = parser.number_of_pages
-    # number_of_pages = 2
-    # if LianJiaWangItemParser.UNKNOWN_PAGE != number_of_pages:
-    #     for page in range(2, number_of_pages+1):
-    #         page_url = '%spg%d' % (url, page)
-    #         page_req = request.Request(page_url, headers=headers)
-    #         page_resp = request.urlopen(page_req)
-    #         page_content = page_resp.read().decode('utf-8')
     #
-    #         page_parser = LianJiaWangItemParser()
-    #         page_parser.feed(page_content)
-    #         page_parser.close()
-    #         print('---- page %d parsed ----\n' % page)
+    parser = LianJiaWangItemParser()
+
+    number_of_pages = 1
+    page = 1
+    while page <= number_of_pages:
+        url = "https://bj.lianjia.com/zufang/pg%d" % page
+        req = request.Request(url, headers=headers)
+        resp = request.urlopen(req)
+        content = resp.read().decode('utf-8')
+
+        parser.feed(content)
+        parser.close()
+        print('pages: %d/%d\n' % (page, number_of_pages))
+
+        page += 1
+        # TODO 调试时只想抓取一页数据，非调试模式请打开注释
+        # number_of_pages = parser.number_of_pages
+        if LianJiaWangItemParser.UNKNOWN_PAGE == number_of_pages:
+            number_of_pages = page - 1  # 中断循环
 
 
 # if __name__ == 'main':
